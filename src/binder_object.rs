f3168f295d7dcb705fdb95d21eb9a27fe586d993
Schmarni <marnistromer@gmail.com>
2026-05-17 05:27:04 +0200

    Revert "refactor: polish owned binder ref"
    This reverts commit 3b3cf21ee04f88c35a018f13f0e113cacb118056.

--- src/binder_object.rs
+++ src/binder_object.rs
@@ -302,7 +302,7 @@ impl Clone for BorrowedBinderObject {
         }
         Self {
             device: self.device.clone(),
-            id: self.id,
+            id: self.id.clone(),
             handler: self.handler.clone(),
         }
     }
@@ -630,7 +630,7 @@ impl<T: TransactionHandler> Clone for BinderObjectRef<T> {
         }
         Self {
             device: self.device.clone(),
-            id: self.id,
+            id: self.id.clone(),
             handler: self.handler.clone(),
         }
     }
@@ -704,17 +704,17 @@ impl<H: TransactionHandler> BinderObjectRef<H> {
         &self.device
     }
 }
-trait OwnedBinderObjectSeal {}
+trait Seal {}
 #[expect(private_bounds)]
-pub trait OwnedBinderObjectRef: ToBinderObjectOrRef + OwnedBinderObjectSeal {
+pub trait OwnedBinderObjectRefTrait<T: TransactionHandler>: ToBinderObjectOrRef + Seal {
     fn to_object_or_ref(&self) -> BinderObjectOrRef {
         self.to_binder_object_or_ref()
     }
 }
-impl<T: TransactionHandler> OwnedBinderObjectSeal for BinderObject<T> {}
-impl<T: TransactionHandler> OwnedBinderObjectRef for BinderObject<T> {}
-impl<T: TransactionHandler> OwnedBinderObjectSeal for BinderObjectRef<T> {}
-impl<T: TransactionHandler> OwnedBinderObjectRef for BinderObjectRef<T> {}
+impl<T: TransactionHandler> Seal for BinderObject<T> {}
+impl<T: TransactionHandler> OwnedBinderObjectRefTrait<T> for BinderObject<T> {}
+impl<T: TransactionHandler> Seal for BinderObjectRef<T> {}
+impl<T: TransactionHandler> OwnedBinderObjectRefTrait<T> for BinderObjectRef<T> {}
 impl ToBinderObjectOrRef for WeakBinderObject {
     fn to_binder_object_or_ref(&self) -> BinderObjectOrRef {
         BinderObjectOrRef::WeakObject(WeakBinderObject {
